a7d967ccab58fc5763a56232fe2afca22bc519ae
Hermios <nicoblex@yahoo.fr>
2024-03-26 16:23:00 +0200

    init

--- constants.lua
+++ constants.lua
@@ -1,5 +1,5 @@
-modname=LTN_Status_on_logistic_network
-tech="tech-"..modname
-recipe="recipe-"..modname
-signal="signal-"..modname
-prototype="prototype-"..modname
+modname="LTN_Status_on_logistic_network"
+signal_dispatcher_no_train_found="ltn-".."dispatcher-no-train-found"
+signal_provider_missing_cargo="ltn-".."provider-missing-cargo"
+signal_provider_unscheduled_cargo="ltn-".."provider-unscheduled-cargo"
+signal_requester_unscheduled_cargo="ltn-".."requester-unscheduled-cargo"
--- prototypes/signal.lua
+++ prototypes/signal.lua
@@ -1,11 +1,35 @@
 data:extend(
 {
   {
-    type = "signal",
-    name = signal,
-    icon = "__"..modname.."__/graphics/icons/signal/signal.png",
-    icon_size = 128,
-    subgroup = "",
+    type = "virtual-signal",
+    name = signal_dispatcher_no_train_found,
+    icon = "__"..modname.."__/graphics/icons/signal/dispatcher-no-train-found.png",
+    icon_size = 64,
+    subgroup = "LTN-signal",
+    order = ""
+  },
+  {
+    type = "virtual-signal",
+    name = signal_provider_missing_cargo,
+    icon = "__"..modname.."__/graphics/icons/signal/provider-missing-cargo.png",
+    icon_size = 64,
+    subgroup = "LTN-signal",
+    order = ""
+  },
+  {
+    type = "virtual-signal",
+    name = signal_provider_unscheduled_cargo,
+    icon = "__"..modname.."__/graphics/icons/signal/provider-unscheduled-cargo.png",
+    icon_size = 64,
+    subgroup = "LTN-signal",
+    order = ""
+  },
+  {
+    type = "virtual-signal",
+    name = signal_requester_unscheduled_cargo,
+    icon = "__"..modname.."__/graphics/icons/signal/requester-unscheduled-cargo.png",
+    icon_size = 64,
+    subgroup = "LTN-signal",
     order = ""
   }
 })
